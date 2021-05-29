74d30d5cc9fbd8037bbcee4f03647bf1d64366f3
Alessandro Lazzara <fenomeno_8_3@hotmail.com>
2021-05-29 12:18:59 +0200

    Aggiungere i file di progetto.

---
+++ GraphQL.Globalization.Api/Constants.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GraphQL.Globalization.Api
+{
+    public static class ApiMessages
+    {
+        public const string ApiMethodLog = "ID: {0}. Called {1} with execution time {2} ms";
+        public const string ApiErrorLog = "ID: {0}. Error for method {1} with execution time {2} ms and request:\n{3}";
+        public const string ApiMethodLogWithBodyRequest = "ID: {0}. Called {1} with execution time {2} ms and request:\n{3}";
+        public const string ApiErrorLogWithBodyRequest = "ID: {0}. Error for method {1} with execution time {2} ms";
+        public const string GraphQlTypeError = "Unable to create generic GraphQL type from type {0} because it has no properties";
+
+
+    }
+
+}
